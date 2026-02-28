3a122046c0a93a0255e5aa750ac4eceff00f11b7
Alar Kume <alarku07@gmail.com>
2026-02-28 20:28:17 +0200

    take be url from env variable

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     hmr: { host: "localhost", port: 80, protocol: "ws" },
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
